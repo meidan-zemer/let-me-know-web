2a06e218027ac17c33aedd53ae23da88c0f19f59
meidan.zemer <meidan@gmail.com>
2019-02-12 14:39:44 +0200

    Add privacy page

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './Components/App';
+import Privacy from './Components/Screens/PrivacyPolicy';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+if(window.location.pathname === '/privacy'){
+    ReactDOM.render(<Privacy />, document.getElementById('root'));
+} else {
+    ReactDOM.render(<App />, document.getElementById('root'));
+}
